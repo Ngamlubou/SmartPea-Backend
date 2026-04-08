99f9d97e7a6af1fe85678084ce96c0be0098f228
Ngamlubou Ringkangmai <ngamluboungamlubou@gmail.com>
2026-04-08 17:13:46 +0530

    Refactor payment routes and integrate Razorpay
    Updated payment processing routes and added order creation functionality.

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 8080;
-const cors = require("cors");
+
 app.use(cors({
   origin: "https://ngamlubou.github.io"
 }));
+
 app.use(express.json());
+
+const razorpay = new Razorpay({
+  key_id: "rzp_test_SYuOBsX5gApP7v",
+  key_secret: process.env.RAZORPAY_SECRET
+});
 
 function generateCode(prefix) {
   const num = Math.floor(Math.random() * 10000)
@@ -14,12 +24,48 @@ function generateCode(prefix) {
   return prefix + num;
 }
 
-app.post("/submit", (req, res) => {
-  const { classValue } = req.body;
-  const code = generateCode(classValue);  
-res.json({
-    success: true, 
-    code: code
-  });
+app.post("/create-order", async (req, res) => {
+  try {
+      const options = {
+  amount: 10000,
+  currency: "INR",
+  receipt: "receipt_" + Date.now()
+};
+ const order = await razorpay.orders.create(options);
+
+   res.json({
+      order_id: order.id,
+      amount: order.amount
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: "Order creation failed" });
+  }
 });
+
+app.post("/verify-payment", (req, res) => {
+    const {
+      classValue,
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature
+    } = req.body;
+
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_SECRET)
+      .update(body.toString())
+      .digest("hex");
+
+    if (expectedSignature !== razorpay_signature) {
+      return res.status(400).json({ error: "Invalid signature" });
+    }
+  const code = generateCode(classValue);
+    res.json({
+      success: true,
+      code: code
+    });
+});
+
 app.listen(PORT);
